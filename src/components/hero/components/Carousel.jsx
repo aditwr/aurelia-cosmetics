0052f7f8560b4e9b3b073baa260da79124076633
adityawahyuramadhan <awahyu148@gmail.com>
2022-08-25 22:44:08 +0700

    try animejs

--- src/components/hero/components/Carousel.jsx
+++ src/components/hero/components/Carousel.jsx
@@ -11,6 +11,29 @@ import Particle from "./Particles";
 export default class Carousel extends Component {
   componentDidMount() {
     // carousel animation
+    const timeline = anime.timeline({});
+    timeline
+      .add({
+        targets: ".carousel-front-backdrop",
+        width: 0,
+        duration: 500,
+        easing: "linear",
+      })
+      .add({
+        targets: ".heading1",
+        opacity: 1,
+        duration: 500,
+      })
+      .add({
+        targets: ".heading2",
+        opacity: 1,
+        duration: 300,
+      })
+      .add({
+        targets: ".cta",
+        opacity: 1,
+        duration: 200,
+      });
   }
   render() {
     return (
@@ -25,18 +48,18 @@ export default class Carousel extends Component {
         {/* container */}
         <div className="px-5 lg:px-0 pt-6 lg:w-[1200px] lg:mx-auto text-left relative z-20">
           <div className="lg:w-7/12 md:pt-4 lg:pt-16">
-            <h1 className="text-3xl lg:text-[40px] font-bold font-lato mb-6 leading-tight">
+            <h1 className="heading1 text-3xl lg:text-[40px] font-bold font-lato mb-6 leading-tight opacity-0">
               Young cosmetics always with you, use best skin care products to
               change your life.
             </h1>
-            <p className="w-10/12 md:w-8/12 text-base font-medium font-montserrat mb-8 text-slate-700">
+            <p className="heading2 w-10/12 md:w-8/12 text-base font-medium font-montserrat mb-8 text-slate-700 opacity-0">
               Young cosmetics base on skin care, so it make you feel your skin
               are most comfortable
             </p>
             {/* call-to-action */}
             <a
               href="example.com"
-              className="font-montserrat font-semibold text-slate-800 px-3 py-2 bg-palleteTenderness group transition-all duration-300 hover:bg-palleteBase hover:rounded"
+              className="cta font-montserrat font-semibold text-slate-800 px-3 py-2 bg-palleteTenderness group transition-all duration-300 hover:bg-palleteBase hover:rounded opacity-0"
             >
               <FontAwesomeIcon
                 icon={faShoppingCart}
@@ -52,7 +75,7 @@ export default class Carousel extends Component {
             className="absolute z-0 right-0 top-0 md:bottom-0 w-48 sm:w-64 md:w-80 lg:w-[460px] translate-y-[324px] sm:translate-y-[240px] md:translate-y-[196px] lg:translate-y-[46px] md:-translate-x-[16%] lg:translate-x-0 transition-all"
           />
           {/* rectangle */}
-          <div className="w-[690px] lg:w-[800px] h-96 lg:h-[436px] bg-white -rotate-[58deg] -z-10 absolute right-0 top-0 translate-x-[68%] sm:translate-x-[50%] md:translate-x-[32%] lg:translate-x-[214px] translate-y-[16%] md:translate-y-0 lg:-translate-y-[24px] transition-all shadow-xl"></div>
+          <div className="rectangle w-[690px] lg:w-[800px] h-96 lg:h-[436px] bg-white -rotate-[58deg] -z-10 absolute right-0 top-0 translate-x-[68%] sm:translate-x-[50%] md:translate-x-[32%] lg:translate-x-[214px] translate-y-[16%] md:translate-y-0 lg:-translate-y-[24px] transition-all shadow-xl"></div>
         </div>
         {/* acsent-img */}
         <img
